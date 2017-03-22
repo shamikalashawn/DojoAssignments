268839de0e5d30ff9da5797e73625221a27c64db
Shamika La Shawn <Sg3147@utsnyc.edu>
2017-03-22 12:41:02 -0400

    working on js circles and users

---
+++ WebFundamentals/Javascript/Users/test.js
@@ -0,0 +1,11 @@
+for (var i = 60; i >= 0; i--) {
+  if (i > 30)
+  {console.log(i + " days until my birthday.");
+} else if (i <= 30)
+  {console.log("It's only " + i + " days until my bday");
+} else if (i <= 5)
+{console.log(i + " DAYS!!!");
+} else if (i === 0)
+ {console.log("IT'S MY MF'N BDAY!!!");
+  }
+} 
