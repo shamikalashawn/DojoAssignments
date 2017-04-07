9f72d9c8d976d2a578e2b74ca28db009b0f31bee
Shamika La Shawn <Sg3147@utsnyc.edu>
2017-04-07 01:01:43 -0400

    Removed 1942, Debugging jQuery, js Circles, and js Scroll

--- Algorithms/Chapter 2/Date, on a Deserted Island.js
+++ Algorithms/Chapter 2/Date, on a Deserted Island.js
@@ -34,8 +34,8 @@ function weekdayName(weekdayNum) {
 
 function weekdayName2(dayNum) {
   var day;
-  if (dayNum > 366 || dayNum < 1) {
-    console.log('That number is out of range.  Try something between 1 and 365.');
+  if (dayNum < 1) {
+    console.log('That number is out of range.  Try something greater than 1.');
     return null;
   } else {
     switch (dayNum % 7) {
@@ -100,15 +100,20 @@ function monthName(monthNum){
 
 // 5) Now expand monthName() to create monthToDays(monthNum), returning the number of days in that month, in the year 2017. Hint: use a SWITCH statement for the days in each month.
 
-function monthToDays(monthNum) {
+function monthToDays(monthNum, leap) {
   var days;
   switch (monthNum) {
     case 1:
       days = 31;
       break;
     case 2:
+    if (leap) {
+      days = 29
+      break;
+    } else{
       days = 28;
       break;
+    }
     case 3:
       days = 31;
       break;
@@ -143,35 +148,35 @@ function monthToDays(monthNum) {
   return days;
 }
 
-// console.log(monthToDays(2));
+// console.log(monthToDays(2, false));
 // 6) Despite using his ember expertise to create a glowing SOS visible from space, the days go by and sadly Eduardo is still not rescued.  Is it spring yet? It might as well be. Build on monthName() to create dayToMonth(dayNum). If given a day number since the year began, return the current month (assuming it is not a Leap year). Given 75, return 'March'.
 
-function dayToMonth(dayNum) {
+function dayToMonth(dayNum, leap) {
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   var counter = 0;
   var sum = 0;
   while (sum < dayNum) {
-    sum += monthToDays(counter + 1);
+    sum += monthToDays(counter + 1, leap);
     console.log(sum);
     counter += 1
   }
   return months[counter-1];
 }
 
-// console.log(dayToMonth(75));
+// console.log(dayToMonth(75, false));
 
 // 7) Eduardo builds a Dojo bootcamp on the island. Initially his students only find Ninja Gold in caves, but eventuallly even his tree sloths can write code quickly! Dojo classes meet Monday thru Friday, so let's reincorporate weekday to our calculations. Construct fullDate(dayNum) to accept number of days so far in 2017, and return a full date string. He hardly remembers that fateful New Year's Eve party, but he knows it was a Saturday. Given 142, return "Monday, May 22, 2017".
 
 function fullDate(dayNum) {
   var day = weekdayName2(dayNum);
   var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  var month = dayToMonth(dayNum);
+  var month = dayToMonth(dayNum, false);
   var monthIdx = months.indexOf(month);
   var date;
   var tempDays = 0;
   var temp;
   for (var i = 1; i <= monthIdx; i++) {
-    temp = monthToDays(i);
+    temp = monthToDays(i, false);
     tempDays += temp;
   }
   date = dayNum - tempDays;
@@ -181,6 +186,74 @@ function fullDate(dayNum) {
 // console.log(fullDate(142));
 // 8) Time flies when you're at a Dojo - months in fact. Build fullDate2(dayNum) that will be given a 4-digit integer: the days that have passed since December 31, 2016. This number can stretch into future years! You can assume that any year number divisible by four is a leap year and has a 29-day February. Given 8475, return "Thursday, March 15, 2040".
 
+function fullDate2(dayNum) {
+  var year = 2017
+  var leap;
+  while (dayNum > 365){
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+      dayNum -= 366
+    }
+    else {
+      dayNum -= 365
+    }
+    year ++
+  }
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+    leap = true;
+  } else {
+    leap = false;
+  }
 
 
+  var day = weekdayName2(dayNum);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var month = dayToMonth(dayNum, leap);
+  var monthIdx = months.indexOf(month);
+  var date;
+  var tempDays = 0;
+  var temp;
+  for (var i = 1; i <= monthIdx; i++) {
+    temp = monthToDays(i, leap);
+    tempDays += temp;
+  }
+  date = dayNum - tempDays;
+  return day + ', ' + month + ' ' + date + ', ' + year
+}
+console.log(fullDate2(8475));
 // 9) Eduardo hacks the Google Maps API and adds this long-forgotten island back onto the map. Soon he is rescued! News of his Hemingway-like stoicism make him famous for centuries. Build fullDate3(dayNum) to handle days up to 140,000! Note: years 2100, 2200, and 2300 are not leap years (although 2400 is). Given 139947, return "Tuesday, February 29, 2400".
+
+function fullDate3(dayNum) {
+  var day = weekdayName2(dayNum);
+  var year = 2017
+  var leap;
+  while (dayNum > 365){
+    if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+      dayNum -= 366;
+      console.log('year', year, 'daynum', dayNum, 'leap');
+    }
+    else {
+      dayNum -= 365;
+      console.log('year', year, 'daynum', dayNum, 'not leap');
+    }
+    year ++
+  }
+  if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0){
+    leap = true;
+  } else {
+    leap = false;
+  }
+  console.log(leap);
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var month = dayToMonth(dayNum, leap);
+  var monthIdx = months.indexOf(month);
+  var date;
+  var tempDays = 0;
+  var temp;
+  for (var i = 1; i <= monthIdx; i++) {
+    temp = monthToDays(i, leap);
+    tempDays += temp;
+  }
+  date = dayNum - tempDays;
+  return day + ', ' + month + ' ' + date + ', ' + year
+}
+console.log(fullDate3(139947));
